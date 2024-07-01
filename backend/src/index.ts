3dd101cff01c6c2ea84d95295ddc044f93217346
Paulo Cesar <paulocgsantos99@gmail.com>
2024-07-01 16:28:24 -0300

    fetch test unit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,17 @@ import "dotenv/config";
 import mongoose from 'mongoose';
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
+import path from "path";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+mongoose
+.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(() => 
+console.log(
+    "Connected to database: ",
+    process.env.MONGODB_CONNECTION_STRING
+)
+)
 
 const app = express();
 app.use(cookieParser());
@@ -18,6 +26,8 @@ app.use(
         credentials: true,
     })
 );
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes)
 app.use('/api/users', userRoutes)
