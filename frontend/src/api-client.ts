b6817428307738d69feeaae1132b721d29ec585b
Paulo Cesar <paulocgsantos99@gmail.com>
2024-07-05 13:04:41 -0300

    added hotel detail page and tests e2e

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -154,4 +154,12 @@ export const searchHotels = async (searchParams:SearchParams): Promise<HotelSear
       }
     
       return response.json();
+};
+
+export const fetchHotelById = async(hotelId: string)=> {
+    const response = await fetch(`${API_BASE_URL}/api/hotels/${hotelId}`)
+    if(!response.ok){
+        throw new Error('Error fetching Hotels');
+    }
+    return response.json();
 }
